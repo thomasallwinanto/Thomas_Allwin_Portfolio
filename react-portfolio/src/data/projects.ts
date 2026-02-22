c999f9164575b8ce78c5c93ebef742e9ff71a02c
thomasallwin <68068162+thomasallwin@users.noreply.github.com>
2026-02-22 19:59:24 +0100

    feat: add Netlify configuration and update project image path

--- react-portfolio/src/data/projects.ts
+++ react-portfolio/src/data/projects.ts
@@ -36,7 +36,7 @@ export const personalProjects = {
   guild: {
     title: 'Guild and Company',
     desc: "I'm part of a collaborative music and creative project in Hamburg where we explore Blues plus 70s and 80s rock. I personally tilt towards the blues, drawing inspiration from Eric Clapton and Stevie Ray Vaughan. I play a Telecaster strung with 10 gauge strings because I find it versatile, simple, and clean, and I'm currently experimenting with wah pedals to play a few tracks and learn more. Follow us on IG: <a href='https://www.instagram.com/theguild_band/' target='_blank' style='color:#00bcd4; text-decoration:underline;'>@theguild_band</a>.",
-    images: ['/assets/tom_guitar.jpg', '/assets/guild.jpg']
+    images: ['/assets/Tom_Guitar.jpg', '/assets/guild.jpg']
   },
   imaginary: {
     title: 'Imaginary Axis',
